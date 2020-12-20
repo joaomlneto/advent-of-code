ef74526918767de2e687a7539af817e30cb57e5f
João Neto <joao@neto.pt>
2020-12-20 17:01:15 +0100

    day 20 - part 2

--- 2020/20/main.js
+++ 2020/20/main.js
@@ -4,6 +4,12 @@ const filename = 'input.txt'
 const file = fs.readFileSync(filename).toString('utf8')
 console.log('filename:', filename)
 
+const seaMonster = [
+  "                  # ",
+  "#    ##    ##    ###",
+  " #  #  #  #  #  #   ",
+].map(row => row.split(''))
+
 const baseTiles = file
   .split('\n\n')
   .map(tile => tile.split('\n').filter(line => line.length > 0))
@@ -11,23 +17,22 @@ const baseTiles = file
     id: Number(tile[0].match(/^Tile (\d+):/)[1]),
     image: tile.slice(1).map(row => row.split('')),
   }))
-  // we pre-compute all the possible arrangements of the tile image
   .map(tile => ({
     ...tile,
-    arrangements: getImageArrangements(tile.image),
+    arrangements: getTileArrangements(tile),
   }))
 
 // precompute which tiles can be adjacent to which and where
-const tiles = baseTiles.map(tile => ({
-  ...tile,
-  adjacents: baseTiles
-    .filter(otherTile =>
-      otherTile.id != tile.id && tilesMayBeAdjacent(tile, otherTile))
-    .map(tile => tile.id)
-}))
+const tiles = baseTiles
+  .map(tile => ({
+    ...tile,
+    adjacents: baseTiles
+      .filter(otherTile =>
+        otherTile.id != tile.id && tilesMayBeAdjacent(tile, otherTile))
+      .map(tile => tile.id)
+  }))
+  .sort((a, b) => a.adjacents.length - b.adjacents.length)
 
-console.log('num tiles:', tiles.length)
-console.log('arrangement side length:', Math.sqrt(tiles.length))
 
 function createSquareMatrix(n) {
   return Array.from({length:n}, () => new Array(n))
@@ -35,62 +40,61 @@ function createSquareMatrix(n) {
 
 function print_tile(tile) {
   console.log('\nTile', tile.id)
-  for (row of tile.image) {
+  for (const row of tile.image) {
     console.log(row.join(''))
   }
 }
 
 function print_image(image) {
-  for (row of image) console.log(row.join(''))
+  for (const row of image) console.log(row.join(''))
 }
 
-function flipImageHorizontally(image) {
-  return image.map(row => row.slice().reverse())
+function flipTileHorizontally(tile) {
+  return {
+    ...tile,
+    image: image.map(row => row.slice().reverse())
+  }
 }
 
-function flipImageVertically(image) {
-  return image.slice().reverse()
+function flipImageVertically(tile) {
+  return {
+    ...tile,
+    image: tile.image.slice().reverse()
+  }
 }
 
-function rotateImage90Degrees(image) {
+function rotateTile90Degrees(tile) {
   let newImage = []
-  for (let i = 0; i < image.length; i++) {
+  for (let i = 0; i < tile.image.length; i++) {
     newImage[i] = []
-    for (let j = 0; j < image[i].length; j++) {
-      newImage[i][j] = image[(image[i].length - 1) - j][i]
+    for (let j = 0; j < tile.image[i].length; j++) {
+      newImage[i][j] = tile.image[(tile.image[i].length - 1) - j][i]
     }
   }
 
-  return newImage
+  return {
+    ...tile,
+    image: newImage,
+  }
 }
 
-function getAllImageRotations(image) {
-  let rotations = [image]
+function getAllTileRotations(tile) {
+  let rotations = [tile]
   for (let i = 0; i < 3; i++)
-    rotations.push(rotateImage90Degrees(rotations[rotations.length - 1]))
+    rotations.push(rotateTile90Degrees(rotations[rotations.length - 1]))
   return rotations
 }
 
-function getImageArrangements(image) {
+function getTileArrangements(tile) {
   return [
-    ...getAllImageRotations(image),
-    ...getAllImageRotations(flipImageVertically(image))
+    ...getAllTileRotations(tile),
+    ...getAllTileRotations(flipImageVertically(tile))
   ]
 }
 
-function getTileArrangements(tile) {
-  return tile.arrangements
-    .map(image => ({
-      ...tile,
-      image,
-    }))
-}
-
-function getAllArrangementsOfTiles(tiles) {
-  return tiles.map(tile => getTileArrangements(tile)).flat()
-}
-
-function imagesMatchHorizontally(leftImage, rightImage) {
+function tilesMatchHorizontally(leftTile, rightTile) {
+  const leftImage = leftTile.image
+  const rightImage = rightTile.image
   for (let i = 0; i < leftImage.length; i++) {
     if (leftImage[i][leftImage[i].length - 1] !== rightImage[i][0])
       return false
@@ -98,7 +102,9 @@ function imagesMatchHorizontally(leftImage, rightImage) {
   return true
 }
 
-function imagesMatchVertically(topImage, bottomImage) {
+function tilesMatchVertically(topTile, bottomTile) {
+  const topImage = topTile.image
+  const bottomImage = bottomTile.image
   for (let i = 0; i < bottomImage[0].length; i++) {
     if (topImage[topImage.length - 1][i] !== bottomImage[0][i])
       return false
@@ -106,104 +112,145 @@ function imagesMatchVertically(topImage, bottomImage) {
   return true
 }
 
-function getHorizontallyMatchingTiles(imageToTheLeft, tiles) {
-  return tiles.map(tile =>
-    tile.arrangements
-      .filter(tileImage => imagesMatchHorizontally(imageToTheLeft, tileImage))
-      .map(image => ({
-        ...tile,
-        image,
-      })))
-    .flat()
-}
-
-function getVerticallyMatchingTiles(imageToTheTop, tiles) {
-  return tiles.map(tile =>
-    tile.arrangements
-      .filter(tileImage => imagesMatchVertically(imageToTheTop, tileImage))
-      .map(image => ({
-        ...tile,
-        image,
-      })))
-    .flat()
-}
-
-function tileMatchesTopImage(imageToTheTop, tile) {
-  for (const arrangement of tile.arrangements) {
-    if (imagesMatchVertically(imageToTheTop, arrangement)) return true
-  }
-  return false
-}
-
-function tileMatchesLeftImage(imageToTheLeft, tile) {
-  for (const arrangement of tile.arrangements) {
-    if (imagesMatchHorizontally(imageToTheLeft, arrangement)) return true
-  }
-  return false
-}
-
 function tilesMayBeAdjacent(tile1, tile2) {
   for (const a1 of tile1.arrangements) {
     for (const a2 of tile2.arrangements) {
-      if (imagesMatchHorizontally(a1, a2)) return true
-      if (imagesMatchVertically(a1, a2)) return true
+      if (tilesMatchHorizontally(a1, a2)) return true
     }
   }
   return false
 }
 
-function log(depth, ...msgs) {
- if (depth < 11) console.log(" |  ".repeat(depth), ...msgs)
+function tileMatchesImageOnTheLeft(leftImage, tile) {
+  for (const arrangement of tile.arrangements) {
+    if (tilesMatchHorizontally({image: leftImage}, tile))
+      return true
+  }
+  return false
+}
+
+function tileMatchesImageOnTheTop(topImage, tile) {
+  for (const arrangement of tile.arrangements) {
+    if (tilesMatchVertically({image: topImage}, tile))
+      return true
+  }
+  return false
 }
 
 function searchSquareArrangement(tiles, arrangement, cellIndex = 0) {
   const numTotalTiles = arrangement.length * arrangement[0].length
-  const numRemainingSpaces = numTotalTiles - cellIndex
-
   const line = Math.floor(cellIndex / arrangement.length)
   const column = cellIndex % arrangement.length
 
-  // console.log('numTotalTiles', numTotalTiles)
-  // console.log('numRemainingTiles', tiles.length)
-  // console.log('numRemainingSpaces', numRemainingSpaces)
-  // console.log('current Tile index', cellIndex, line, column)
+  if (cellIndex === numTotalTiles) return arrangement
+  if (tiles.length < numTotalTiles - cellIndex) return false
 
-  if (numRemainingSpaces === 0) {
-    console.log('HABEMUS ARRANGEMENT!')
-    for (let row = 0; row < arrangement.length; row++) {
-      console.log(arrangement[row].map(tile => tile.id))
-    }
-    throw new Error("Success!!");
-  }
+  let possibleTiles = tiles
 
-  if (tiles.length < numRemainingSpaces) {
-    log(cellIndex, 'Not enough tiles')
-    return
-  }
-
-  // compute which tiles can be placed in the current position
-  let possibleTiles = tiles;
   if (line > 0) {
     const topTile = arrangement[line - 1][column]
-    possibleTiles = possibleTiles.filter(tile => topTile.adjacents.includes(tile.id))
+    possibleTiles = possibleTiles
+      .filter(tile => topTile.adjacents.includes(tile.id))
+      .filter(tile => tileMatchesImageOnTheTop(topTile.image, tile))
   }
 
   if (column > 0) {
     const leftTile = arrangement[line][column - 1]
-    possibleTiles = possibleTiles.filter(tile => leftTile.adjacents.includes(tile.id))
+    possibleTiles = possibleTiles
+      .filter(tile => leftTile.adjacents.includes(tile.id))
+      .filter(tile => tileMatchesImageOnTheLeft(leftTile.image, tile))
   }
 
-  if (possibleTiles.length > 2)
-    log(cellIndex, 'Position', line, column, 'possibilities:', possibleTiles.length)
   for (const tile of possibleTiles) {
     arrangement[line][column] = tile
     const remainingTiles = tiles.filter(t => t.id != tile.id)
-    searchSquareArrangement(remainingTiles, arrangement, cellIndex + 1)
+    const found = searchSquareArrangement(remainingTiles, arrangement, cellIndex + 1)
+    if (found) return found
   }
 }
 
-console.log(tiles.filter(tile => tile.adjacents.length < 4).map(tile => "TILE " + tile.id + " #Adjacents = " + tile.adjacents.length))
+function getCornerTiles(arrangement) {
+  return [
+    arrangement[0][0],
+    arrangement[arrangement.length - 1][0],
+    arrangement[0][arrangement[0].length - 1],
+    arrangement[arrangement.length - 1][arrangement[arrangement.length - 1].length - 1],
+  ]
+}
 
-const side_length = Math.sqrt(tiles.length)
-const arrangement = Array.from({length:side_length}, () => new Array(side_length))
-searchSquareArrangement(getAllArrangementsOfTiles(tiles), arrangement)
+function convertTilesIntoImage(arrangement) {
+  const x = arrangement
+    .map((row, i) => {
+      const [firstRowImage, ...otherRowImages] = row.map(tile => tile.image)
+      return firstRowImage.map((line, i) => [
+        ...line.slice(1, -1),
+        ...otherRowImages.map(image => image[i].slice(1, -1)).flat()
+      ]).slice(1, -1)
+    })
+    .flat()
+  // FIXME: Remove the rotate/flip.
+  return rotateTile90Degrees(flipImageVertically({image:x})).image
+}
+
+function markSeaMonster(image, row, col) {
+  for (let i = 0; i < seaMonster.length; i++) {
+    for (let j = 0; j < seaMonster[i].length; j++) {
+      if (seaMonster[i][j] === '#') image[row+i][col+j] = 'X'
+    }
+  }
+  return image
+}
+
+function checkSeaMonster(image, row, col) {
+  for (let i = 0; i < seaMonster.length; i++) {
+    for (let j = 0; j < seaMonster[i].length; j++) {
+      if (seaMonster[i][j] === '#' && image[row+i][col+j] !== '#') return false
+    }
+  }
+  return true
+}
+
+function scanImageForSeaMonsters(image) {
+  let seaMonsterCoordinates = []
+  for (let row = 0; row < image.length - seaMonster.length + 1; row++) {
+    for (let col = 0; col < image[row].length - seaMonster[0].length + 1; col++) {
+      const x = checkSeaMonster(image, row, col)
+      if (x) seaMonsterCoordinates.push([row, col])
+    }
+  }
+  return seaMonsterCoordinates
+}
+
+function findAndMarkSeaMonsters(image) {
+  const imageArrangements =
+    getTileArrangements({image:image}).map(tile => tile.image)
+  for (const image of imageArrangements) {
+    const coordinates = scanImageForSeaMonsters(image)
+    if (coordinates.length > 0) {
+      coordinates.forEach(([row, col]) => markSeaMonster(image, row, col))
+      return image
+    }
+  }
+}
+
+
+function getHabitatWaterRoughness(image) {
+  return image.reduce((acc, row) =>
+    acc + row.reduce((acc, cell) =>
+      acc + ((cell === '#') ? 1 : 0),
+      0),
+    0)
+}
+
+
+const allTiles = tiles.map(tile => getTileArrangements(tile)).flat()
+const initialArrangement = createSquareMatrix(Math.sqrt(tiles.length))
+const arrangement = searchSquareArrangement(allTiles, initialArrangement)
+
+console.log('Part 1 =', getCornerTiles(arrangement)
+  .map(tile => tile.id)
+  .reduce((a, b) => a * b, 1))
+
+const image = convertTilesIntoImage(arrangement)
+const markedImage = findAndMarkSeaMonsters(image)
+console.log('Part 2 =', getHabitatWaterRoughness(markedImage))
