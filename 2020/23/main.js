17ee710d7addfd81438958d72e843f13abd88ccb
João Neto <joao@neto.pt>
2020-12-23 15:57:58 +0100

    day 23

--- 2020/23/main.js
+++ 2020/23/main.js
@@ -1,142 +1,118 @@
 const fs = require('fs')
 
-const filename = 'sample.txt'
+const filename = 'input.txt'
 const file = fs.readFileSync(filename).toString('utf8')
 console.log('filename:', filename)
+
+class Cup {
+  constructor(label, next) {
+    this.label = label
+    if (!next) {
+      this.next = this
+      this.prev = this
+    } else {
+      this.prev = next.prev
+      this.next = next
+      this.prev.next = this
+      this.next.prev = this
+    }
+  }
+
+  // places nodes after the current element
+  // cup <-> cups[0] <-> ... <-> cups[n-1] <-> cup.next
+  // NOTE: we assume the nodes in the list have their prev/next pointers
+  //       set up correctly and only the edge nodes need to be updated
+  insertAfter(cups) {
+    cups[0].prev = this
+    cups[cups.length - 1].next = this.next
+    this.next.prev = cups[cups.length - 1]
+    this.next = cups[0]
+  }
+
+  extractAfter(amount) {
+    let extracted = []
+    let nextCup = this.next
+    for (let i = 0; i < amount; i++) {
+      extracted = [...extracted, nextCup]
+      nextCup = nextCup.next
+    }
+    this.next = nextCup
+    nextCup.prev = this
+    return extracted
+  }
+}
+
+class CupsCircle {
+  constructor(cupLabels) {
+    this.head = null
+    this.length = 0
+    this.labelLookup = {}
+    cupLabels.forEach(label => this.insertCup(label))
+  }
+
+  insertCup(label) {
+    const cup = new Cup(label, this.head)
+    if (!this.head) this.head = cup
+    this.labelLookup[label] = cup
+    this.length++
+  }
+
+  getCupWithLabel(label) {
+    return this.labelLookup[label]
+  }
+
+  destinationCup(currentCup, pickedUpCups) {
+    const pickedUpLabels = pickedUpCups.map(cup => cup.label)
+    let destinationCup = currentCup.label
+    do {
+      destinationCup = mod(destinationCup - 2, this.length) + 1
+    } while (pickedUpLabels.includes(destinationCup))
+    return this.getCupWithLabel(destinationCup)
+  }
+
+  doTheCrabMove(currentCup) {
+    const pickedUpCups = currentCup.extractAfter(3)
+    const destinationCup = this.destinationCup(currentCup, pickedUpCups)
+    destinationCup.insertAfter(pickedUpCups)
+  }
+
+  playCrabGame(numRounds) {
+    let currentCup = this.head
+    for (let i = 0; i < numRounds; i++) {
+      this.doTheCrabMove(currentCup)
+      currentCup = currentCup.next
+    }
+  }
+
+  toArray() {
+    if (!this.head) return []
+    let labels = [this.head.label]
+    for (let elem = this.head.next; elem !== this.head; elem = elem.next)
+      labels = [...labels, elem.label]
+    return labels
+  }
+}
 
 function mod(value, modulo) {
   return (value + modulo) % modulo
 }
 
-function pickUpCups(cups, currentCupIndex, numCups) {
-  const pickedUpCups = [...Array(numCups)]
-    .map(i => cups.splice(mod(currentCupIndex + 1, cups.length), 1))
-    .flat()
-  return pickedUpCups
+function padCups(cups, len) {
+  return [...cups, ...[...Array(len + 1).keys()].slice(cups.length + 1)]
 }
 
-function placeCups(cups, destinationCupIndex, pickedUpCups) {
-  cups.splice(destinationCupIndex + 1, 0, ...pickedUpCups)
+function playPartOne(cups) {
+  const circle = new CupsCircle(cups)
+  circle.playCrabGame(100)
+  circle.head = circle.getCupWithLabel(1)
+  return Number(circle.toArray().slice(1).join(''))
 }
 
-function getDestinationCup(cups, currentCup, totalCups) {
-  let destinationCup = currentCup
-  do {
-    destinationCup = mod(destinationCup - 2, totalCups + 1) + 1
-  } while (!cups.includes(destinationCup))
-  return destinationCup
-}
-
-function crabMove(cups, currentCupIndex) {
-  const numCups = cups.length
-  // console.log(JSON.stringify(cups).replace(/,/g,' '))
-  const currentCup = cups[currentCupIndex]
-  // console.log('current cup', currentCup)
-
-  const pickedUpCups = pickUpCups(cups, currentCupIndex, 3)
-  //const pickedUpCups = cups.splice(2, 3)
-
-  // pick up 3 cups clockwise of currentCup
-  // console.log('pick up    ', pickedUpCups)
-
-  let destinationCup = getDestinationCup(cups, currentCup, numCups)
-  const destinationCupIndex = cups.indexOf(destinationCup)
-  // console.log('destination', destinationCup)
-
-  placeCups(cups, destinationCupIndex, pickedUpCups)
-}
-
-function padCups(cups) {
-  const max = Math.max(...cups)
-  console.log('highest number is', max)
-  const next = Math.max(...cups)
-  const allNumbers = [...Array(1000001).keys()]
-  return [...cups, ...allNumbers.slice(cups.length + 1)]
-}
-
-function shiftArrayLeft(array) {
-  array.push(array.shift())
-}
-
-function playPartOne(initialCups) {
-  const cups = initialCups.slice()
-  for (let i = 0; i < 100; i++) {
-    //console.log('\n--- move', (i+1), '---')
-    crabMove(cups, 0)
-    shiftArrayLeft(cups)
-  }
-  // return numbers to the right of cup labeled 1
-  while (cups[0] !== 1) shiftArrayLeft(cups)
-  return cups.slice(1).join('')
-}
-
-class ListNode {
-  constructor(value) {
-    this.value = value
-    this.prev = null
-    this.next = null
-  }
-
-  // extract N elements after the current element
-  extractNext(num) {
-    let extracted = []
-    let next = num.next
-    for (let i = 0; i < num; i++) {
-      extracted = [...extracted, next.value]
-      next = next.next
-    }
-    this.next = next
-    next.prev = this
-    return extracted
-  }
-}
-
-class CircularList {
-  constructor(values) {
-    this.head = null
-    this.size = 0
-    for (const value of values) this.insert(value)
-  }
-
-  insert(value) {
-    const elem = new ListNode(value)
-    if (!this.head) {
-      this.head = elem
-      elem.next = elem
-      elem.prev = elem
-    } else {
-      elem.prev = this.head.prev
-      elem.next = this.head
-      elem.prev.next = elem
-      elem.next.prev = elem
-    }
-    this.size++
-  }
-
-  toArray() {
-    let values = []
-    let elem = this.head
-    for (let i = 0; i < this.size; i++) {
-      values = [...values, elem.value]
-      elem = elem.next
-    }
-    return values
-  }
-}
-
-function playPartTwo(initialCups) {
-  const cups = padCups(initialCups.slice())
-  let list = { head: null }
-
-/*
-  for (let i = 0; i < 10000000; i++) {
-    console.log('\n--- move', (i+1), '---')
-    crabMove(cups, 0)
-    shiftArrayLeft(cups)
-  }
-  // return numbers to the right of cup labeled 1
-  while (cups[0] !== 1) shiftArrayLeft(cups)
-  return cups.slice(1).join('')*/
+function playPartTwo(cups) {
+  const circle = new CupsCircle(padCups(cups, 1000000))
+  circle.playCrabGame(10000000)
+  const cupOne = circle.getCupWithLabel(1)
+  return cupOne.next.label * cupOne.next.next.label
 }
 
 const cups = file
@@ -144,17 +120,5 @@ const cups = file
   .split('')
   .map(cup => Number(cup))
 
-
-
-// console.log('Part 1 =', playPartOne(cups))
-// playPartTwo(cups)
-
-console.log(cups)
-
-console.log('....................')
-console.log('....................')
-console.log('....................')
-
-const list = new CircularList(cups)
-
-console.log(list.toArray())
+console.log('Part 1 =', playPartOne(cups))
+console.log('Part 2 =', playPartTwo(cups))
